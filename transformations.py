a67670efde9f61b9efa63f3e3d2083ad00e33b9e
Tobias Brandy <tbrandy@itba.edu.ar>
2022-03-10 16:51:39 -0300

    Better transformation error handling

--- transformations.py
+++ transformations.py
@@ -14,7 +14,16 @@ TR_DIALOG: str = 'tr_dialog'
 TR_NAME_INPUT: str = 'tr_name_input'
 TR_IMG_INPUT: str = 'tr_img_input'
 
-def finalize_transformation(new_image: Image) -> None:
+TrHandler = Callable[[str], Image]
+
+@render_error
+def execute_transformation(image_name: str, handler: TrHandler) -> None:
+    try:
+        new_image = handler(image_name)
+    except Exception as e:
+        dpg.delete_item(TR_DIALOG)
+        raise e
+
     img_repo.persist_image(new_image)
     interface.register_image(new_image)
     interface.render_image_window(new_image.name)
@@ -22,15 +31,14 @@ def finalize_transformation(new_image: Image) -> None:
 def build_tr_dialog(tr_id: str) -> int:
     return dpg.window(label=f'Apply {tr_id.capitalize()} Transformation', tag=TR_DIALOG, modal=True, no_close=True, pos=interface.CENTER_POS)
 
-@render_error
-def delete_dialog() -> None:
-    dpg.delete_item(TR_DIALOG)
-
 def build_tr_name_input(tr_id: str, image_name: str) -> None:
     dpg.add_text('Select New Image Name (no extension)')
     dpg.add_input_text(default_value=strip_extension(image_name) + f'_{tr_id}', tag=TR_NAME_INPUT)
 
-def get_tr_name_value(image: Image) -> str:
+def get_req_tr_name_value(image: Image) -> str:
+    base_name = dpg.get_value(TR_NAME_INPUT)
+    if not base_name:
+        raise ValueError('A name for the new image must be provided')
     return dpg.get_value(TR_NAME_INPUT) + image.format.to_extension()
 
 def build_op_img_selector(image_name: str) -> None:
@@ -44,10 +52,10 @@ def get_req_tr_img_value() -> Image:
         raise ValueError('Selecting a valid image is required for transformation')
     return img_repo.get_image(img_name)
 
-def build_tr_dialog_end_buttons(tr_id: str, image_name: str, handle: Callable[[str], None]) -> None:
+def build_tr_dialog_end_buttons(tr_id: str, image_name: str, handle: TrHandler) -> None:
     with dpg.group(horizontal=True):
-        dpg.add_button(label='Transform', user_data=(handle, image_name), callback=lambda s, ap, ud: ud[0](ud[1]))
-        dpg.add_button(label='Cancel', user_data=tr_id, callback=lambda: delete_dialog())
+        dpg.add_button(label='Transform', user_data=(image_name, handle), callback=lambda s, ap, ud: execute_transformation(*ud))
+        dpg.add_button(label='Cancel', user_data=tr_id, callback=lambda: dpg.delete_item(TR_DIALOG))
 
 
 TR_NOP: str = 'nop'
@@ -56,20 +64,16 @@ def build_nop_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_NOP):
         build_tr_name_input(TR_NOP, image_name)
         # Aca declaramos inputs necesarios para el handle. Este caso no tiene.
-        build_tr_dialog_end_buttons(TR_NOP, image_name, nop_handle)
+        build_tr_dialog_end_buttons(TR_NOP, image_name, tr_nop)
 
-@render_error
-def nop_handle(image_name: str) -> None:
+def tr_nop(image_name: str) -> Image:
     # 1. Obtenemos inputs
     image = img_repo.get_image(image_name)
-    new_name: str = get_tr_name_value(image)
-    # 2. Eliminamos modal
-    delete_dialog()
-    # 3. Procesamos - Deberia ser async
+    new_name: str = get_req_tr_name_value(image)
+    # 2. Procesamos - Puede ser async
     # Do Nothing
-    # 4. Creamos Imagen y finalizamos
-    new_image = Image(new_name, image.format, image.data)
-    finalize_transformation(new_image)
+    # 3. Creamos Imagen
+    return Image(new_name, image.format, image.data)
 
 
 TR_ADD: str = 'add'
@@ -78,24 +82,20 @@ def build_add_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_ADD):
         build_tr_name_input(TR_ADD, image_name)
         build_op_img_selector(image_name)
-        build_tr_dialog_end_buttons(TR_ADD, image_name, sum_handle)
+        build_tr_dialog_end_buttons(TR_ADD, image_name, tr_add)
 
-@render_error
-def sum_handle(image_name: str) -> None:
+def tr_add(image_name: str) -> Image:
     # 1. Obtenemos inputs
     image = img_repo.get_image(image_name)
-    new_name: str = get_tr_name_value(image)
+    new_name: str = get_req_tr_name_value(image)
     sec_image = get_req_tr_img_value()
     if image.shape != sec_image.shape:
         raise ValueError('You can only sum images with the same shape')
 
-    # 2. Eliminamos modal
-    delete_dialog()
-    # 3. Procesamos - Deberia ser async
+    # 2. Procesamos
     new_data = sum_images(image, sec_image)
-    # 4. Creamos Imagen y finalizamos
-    new_image = Image(new_name, image.format, new_data)
-    finalize_transformation(new_image)
+    # 3. Creamos Imagen y finalizamos
+    return Image(new_name, image.format, new_data)
 
 
 TRANSFORMATIONS: Dict[str, Callable[[str], None]] = {
