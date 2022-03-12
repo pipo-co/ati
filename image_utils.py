Hist calculation fix

image_utils.py
@@ -220,11 +220,13 @@ def pollute_gaussian(img: Image, percentage:int, median: float, sigma:float, mod
         new_arr = np.array([get_grey_value(xi, umb) for xi in img.data.flatten()], dtype=np.uint8)
 
 def channel_histogram(channel: np.ndarray) -> np.ndarray:
-    return np.histogram(channel.flatten(), bins=COLOR_DEPTH, range=(0, COLOR_DEPTH)) / channel.size
+    
+    hist, bins = np.histogram(channel.flatten(), bins=COLOR_DEPTH, range=(0, COLOR_DEPTH))
+    return hist / channel.size, bins
 
 def hist_equalization(channel: np.ndarray)  -> np.ndarray:
 
-    normed_hist = channel_histogram(channel)
+    normed_hist, bins = channel_histogram(channel)
     s = normed_hist.cumsum()
     masked_s = np.ma.masked_equal(s, 0)
     masked_s = (masked_s - masked_s.min())*(MAX_COLOR)/(masked_s.max()-masked_s.min())