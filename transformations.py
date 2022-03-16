c8518364af0d42daf323804513c055c8995d3906
Tobias Brandy <tbrandy@itba.edu.ar>
2022-03-16 02:47:54 -0300

    Transformations: copy and reformat now in 'Utils' group

--- transformations.py
+++ transformations.py
@@ -27,29 +27,29 @@ TrHandler = Callable[[str], Image]
 
 def build_transformations_menu(image_name: str) -> None:
     with dpg.menu(label='Transform'):
-        with dpg.menu(label='Basic'):
-            build_tr_menu_item(TR_COPY, build_copy_dialog, image_name)
+        with dpg.menu(label='Utils'):
+            build_tr_menu_item(TR_COPY,     build_copy_dialog, image_name)
             build_tr_menu_item(TR_REFORMAT, build_reformat_dialog, image_name)
-            build_tr_menu_item(TR_NEG, build_neg_dialog, image_name)
-            build_tr_menu_item(TR_POW, build_pow_dialog, image_name)
-            build_tr_menu_item(TR_UMBRAL, build_umbral_dialog, image_name)
+        with dpg.menu(label='Basic'):
+            build_tr_menu_item(TR_NEG,      build_neg_dialog, image_name)
+            build_tr_menu_item(TR_POW,      build_pow_dialog, image_name)
+            build_tr_menu_item(TR_UMBRAL,   build_umbral_dialog, image_name)
             build_tr_menu_item(TR_EQUALIZE, build_equalize_dialog, image_name)
         with dpg.menu(label='Combine'):
-            build_tr_menu_item(TR_ADD, build_add_dialog, image_name)
-            build_tr_menu_item(TR_SUB, build_sub_dialog, image_name)
-            build_tr_menu_item(TR_MULT, build_mult_dialog, image_name)
+            build_tr_menu_item(TR_ADD,      build_add_dialog, image_name)
+            build_tr_menu_item(TR_SUB,      build_sub_dialog, image_name)
+            build_tr_menu_item(TR_MULT,     build_mult_dialog, image_name)
         with dpg.menu(label='Noise'):
-            build_tr_menu_item(TR_GAUSS, build_gauss_dialog, image_name)
-            build_tr_menu_item(TR_EXP, build_exp_dialog, image_name)
+            build_tr_menu_item(TR_GAUSS,    build_gauss_dialog, image_name)
+            build_tr_menu_item(TR_EXP,      build_exp_dialog, image_name)
             build_tr_menu_item(TR_RAYLEIGH, build_rayleigh_dialog, image_name)
-            build_tr_menu_item(TR_SALT, build_salt_dialog, image_name)
+            build_tr_menu_item(TR_SALT,     build_salt_dialog, image_name)
         with dpg.menu(label='Denoise'):
-            build_tr_menu_item(TR_MEAN, build_denoise_mean_dialog, image_name)
-            build_tr_menu_item(TR_MEDIAN, build_denoise_median_dialog, image_name)
+            build_tr_menu_item(TR_MEAN,     build_denoise_mean_dialog, image_name)
+            build_tr_menu_item(TR_MEDIAN,   build_denoise_median_dialog, image_name)
 
 def build_tr_menu_item(tr_id: str, tr_dialog_builder: Callable[[str], None], image_name: str) -> None:
-    dpg.add_menu_item(label=tr_id.capitalize(), user_data=(tr_dialog_builder, image_name),
-                      callback=lambda s, ad, ud: ud[0](ud[1]))
+    dpg.add_menu_item(label=tr_id.capitalize(), user_data=(tr_dialog_builder, image_name), callback=lambda s, ad, ud: ud[0](ud[1]))
 
 @render_error
 def execute_transformation(image_name: str, handler: TrHandler) -> None:
@@ -63,23 +63,21 @@ def execute_transformation(image_name: str, handler: TrHandler) -> None:
     interface.render_image_window(new_image.name)
 
 def build_tr_dialog(tr_id: str) -> int:
-    return dpg.window(label=f'Apply {tr_id.capitalize()} Transformation', tag=TR_DIALOG, modal=True, no_close=True,
-                      pos=interface.CENTER_POS)
+    return dpg.window(label=f'Apply {tr_id.capitalize()} Transformation', tag=TR_DIALOG, modal=True, no_close=True, pos=interface.CENTER_POS)
 
 def build_tr_name_input(tr_id: str, image_name: str) -> None:
     dpg.add_text('Select New Image Name (no extension)')
     dpg.add_input_text(default_value=strip_extension(image_name) + f'_{tr_id}', tag=TR_NAME_INPUT)
 
-def build_tr_value_int_selector(value: str, min: int, max: int, mtag: str = TR_INT_VALUE_SELECTOR) -> None:
+def build_tr_value_int_selector(value: str, min_val: int, max_val: int, m_tag: str = TR_INT_VALUE_SELECTOR) -> None:
     dpg.add_text(f'Select {value} value')
-    dpg.add_input_int(min_value=min, max_value=max, label=f'pick a value for {value} between {min} and {max}', tag=mtag)
+    dpg.add_input_int(min_value=min_val, max_value=max_val, label=f'pick a value for {value} between {min_val} and {max_val}', tag=m_tag)
 
-def build_tr_value_float_selector(value: str, min: float, max: float, mtag: str = TR_FLOAT_VALUE_SELECTOR) -> None:
+def build_tr_value_float_selector(value: str, min_val: float, max_val: float, m_tag: str = TR_FLOAT_VALUE_SELECTOR) -> None:
     dpg.add_text(f'Select {value} value')
-    dpg.add_input_float(min_value=min, max_value=max, label=f'pick a value for {value} between {min} and {max}',
-                        tag=mtag)
+    dpg.add_input_float(min_value=min_val, max_value=max_val, label=f'pick a value for {value} between {min_val} and {max_val}', tag=m_tag)
 
-def build_tr_radiobuttons(names: List[str], default_value: Optional[str] = None) -> None:
+def build_tr_radio_buttons(names: List[str], default_value: Optional[str] = None) -> None:
     names = list(map(str.capitalize, names))
     if default_value:
         default_value = default_value.capitalize()
@@ -124,8 +122,9 @@ def build_tr_dialog_end_buttons(tr_id: str, image_name: str, handle: TrHandler) 
         dpg.add_button(label='Cancel', user_data=tr_id, callback=lambda: dpg.delete_item(TR_DIALOG))
 
 ########################################################
-##################### Basic ############################
+# ******************** Utilities ********************* #
 ########################################################
+
 
 TR_COPY: str = 'copy'
 @render_error
@@ -139,7 +138,7 @@ def tr_nop(image_name: str) -> Image:
     # 1. Obtenemos inputs
     image = img_repo.get_image(image_name)
     new_name: str = get_req_tr_name_value(image)
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     # Do Nothing
     # 3. Creamos Imagen
     return Image(new_name, image.format, image.data)
@@ -149,7 +148,7 @@ TR_REFORMAT: str = 'reformat'
 @render_error
 def build_reformat_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_COPY):
-        build_tr_radiobuttons(ImageFormat.values())
+        build_tr_radio_buttons(ImageFormat.values())
         build_tr_dialog_end_buttons(TR_COPY, image_name, tr_reformat)
 
 def tr_reformat(image_name: str) -> Image:
@@ -157,10 +156,14 @@ def tr_reformat(image_name: str) -> Image:
     image = img_repo.get_image(image_name)
     new_fmt = ImageFormat.from_str(dpg.get_value(TR_RADIO_BUTTONS).lower())
     new_name = strip_extension(image_name) + new_fmt.to_extension()
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     # Do Nothing
     # 3. Creamos Imagen
     return Image(new_name, new_fmt, image.data)
+
+########################################################
+# ********************** Basic *********************** #
+########################################################
 
 
 TR_NEG: str = 'neg'
@@ -174,7 +177,7 @@ def tr_neg(image_name: str) -> Image:
     # 1. Obtenemos inputs
     image = img_repo.get_image(image_name)
     new_name: str = get_req_tr_name_value(image)
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = get_negative(image)
     # Do Nothing
     # 3. Creamos Imagen
@@ -194,7 +197,7 @@ def tr_pow(image_name: str) -> Image:
     image = img_repo.get_image(image_name)
     new_name: str = get_req_tr_name_value(image)
     gamma: float = get_gamma_tr_value()
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = power_function(image, gamma)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
@@ -213,10 +216,11 @@ def tr_umb(image_name: str) -> Image:
     image = img_repo.get_image(image_name)
     new_name: str = get_req_tr_name_value(image)
     umb: int = dpg.get_value(TR_INT_VALUE_SELECTOR)
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = transform_from_threshold(image, umb)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
+
 
 TR_EQUALIZE: str = 'equalize'
 @render_error
@@ -235,17 +239,18 @@ def tr_equalize(image_name: str) -> Image:
     return Image(new_name, image.format, new_data)
 
 ########################################################
-##################### Noise ############################
+# ********************** Noise *********************** #
 ########################################################
+
 
 TR_GAUSS: str = 'gauss'
 @render_error
 def build_gauss_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_GAUSS):
         build_tr_name_input(TR_GAUSS, image_name)
-        build_tr_radiobuttons(['add', 'mult'], 'add')
-        build_tr_value_float_selector('sigma', 0, 1, mtag='sigma_value_input')
-        build_tr_value_float_selector('percentage', 0, 100, mtag='percentage_value_input')
+        build_tr_radio_buttons(['add', 'mult'], 'add')
+        build_tr_value_float_selector('sigma', 0, 1, m_tag='sigma_value_input')
+        build_tr_value_float_selector('percentage', 0, 100, m_tag='percentage_value_input')
         build_tr_dialog_end_buttons(TR_GAUSS, image_name, tr_gauss)
 
 def tr_gauss(image_name: str) -> Image:
@@ -255,7 +260,7 @@ def tr_gauss(image_name: str) -> Image:
     sigma: float = dpg.get_value('sigma_value_input')
     percentage: float = dpg.get_value('percentage_value_input')
     mode: str = dpg.get_value('tr_radio_buttons')
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = pollute_img(image, NoiseType.GAUSS, percentage, sigma, mode)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
@@ -266,9 +271,9 @@ TR_EXP: str = 'exp'
 def build_exp_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_EXP):
         build_tr_name_input(TR_EXP, image_name)
-        build_tr_radiobuttons(['add', 'mult'], 'add')
-        build_tr_value_float_selector('lambda', 0, 1, mtag='parameter_value_input')
-        build_tr_value_float_selector('percentage', 0, 100, mtag='percentage_value_input')
+        build_tr_radio_buttons(['add', 'mult'], 'add')
+        build_tr_value_float_selector('lambda', 0, 1, m_tag='parameter_value_input')
+        build_tr_value_float_selector('percentage', 0, 100, m_tag='percentage_value_input')
         build_tr_dialog_end_buttons(TR_EXP, image_name, tr_exp)
 
 def tr_exp(image_name: str) -> Image:
@@ -278,7 +283,7 @@ def tr_exp(image_name: str) -> Image:
     param: float = dpg.get_value('parameter_value_input')
     percentage: float = dpg.get_value('percentage_value_input')
     mode: str = dpg.get_value('tr_radio_buttons')
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = pollute_img(image, NoiseType.EXP, percentage, param, mode)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
@@ -289,9 +294,9 @@ TR_RAYLEIGH: str = 'rayleigh'
 def build_rayleigh_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_RAYLEIGH):
         build_tr_name_input(TR_RAYLEIGH, image_name)
-        build_tr_radiobuttons(['add', 'mult'], 'add')
-        build_tr_value_float_selector('rayleigh parameter', 0, 1, mtag='rayleigh_value_input')
-        build_tr_value_float_selector('percentage', 0, 100, mtag='percentage_value_input')
+        build_tr_radio_buttons(['add', 'mult'], 'add')
+        build_tr_value_float_selector('rayleigh parameter', 0, 1, m_tag='rayleigh_value_input')
+        build_tr_value_float_selector('percentage', 0, 100, m_tag='percentage_value_input')
         build_tr_dialog_end_buttons(TR_RAYLEIGH, image_name, tr_rayleigh)
 
 def tr_rayleigh(image_name: str) -> Image:
@@ -301,7 +306,7 @@ def tr_rayleigh(image_name: str) -> Image:
     param: float = dpg.get_value('rayleigh_value_input')
     percentage: float = dpg.get_value('percentage_value_input')
     mode: str = dpg.get_value('tr_radio_buttons')
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = pollute_img(image, NoiseType.RAYL, percentage, param, mode)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
@@ -312,7 +317,7 @@ TR_SALT: str = 'salt'
 def build_salt_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_SALT):
         build_tr_name_input(TR_SALT, image_name)
-        build_tr_value_float_selector('percentage', 0, 100, mtag='percentage_value_input')
+        build_tr_value_float_selector('percentage', 0, 100, m_tag='percentage_value_input')
         build_tr_dialog_end_buttons(TR_SALT, image_name, tr_salt)
 
 def tr_salt(image_name: str) -> Image:
@@ -320,14 +325,15 @@ def tr_salt(image_name: str) -> Image:
     image = img_repo.get_image(image_name)
     new_name: str = get_req_tr_name_value(image)
     percentage: float = dpg.get_value('percentage_value_input')
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = salt_img(image, percentage)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
 
 ########################################################
-##################### Combine ##########################
+# ********************* Combine ********************** #
 ########################################################
+
 
 TR_ADD: str = 'add'
 @render_error
@@ -347,6 +353,7 @@ def tr_add(image_name: str) -> Image:
     new_data = add_images(image, sec_image)
     # 3. Creamos Imagen y finalizamos
     return Image(new_name, image.format, new_data)
+
 
 TR_SUB: str = 'sub'
 @render_error
@@ -387,19 +394,18 @@ def tr_mult(image_name: str) -> Image:
     # 3. Creamos Imagen y finalizamos
     return Image(new_name, image.format, new_data)
 
+########################################################
+# ********************* Denoise ********************** #
+########################################################
 
-########################################################
-################### Denoising ##########################
-########################################################
 
 TR_MEAN: str = 'mean'
 @render_error
 def build_denoise_mean_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_MEAN):
         build_tr_name_input(TR_MEAN, image_name)
-        # Aca declaramos inputs necesarios para el handle. Este caso no tiene.
         build_tr_value_int_selector('n', 3, 255)
-        build_tr_radiobuttons(PaddingStrategy.values())
+        build_tr_radio_buttons(PaddingStrategy.values())
         build_tr_dialog_end_buttons(TR_MEAN, image_name, tr_mean)
 
 
@@ -409,7 +415,7 @@ def tr_mean(image_name: str) -> Image:
     new_name: str = get_req_tr_name_value(image)
     n: int = int(dpg.get_value(TR_INT_VALUE_SELECTOR))
     padding: str = dpg.get_value(TR_RADIO_BUTTONS)
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = image.apply_over_channels(mean, n, padding)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
@@ -420,9 +426,8 @@ TR_MEDIAN: str = 'median'
 def build_denoise_median_dialog(image_name: str) -> None:
     with build_tr_dialog(TR_MEDIAN):
         build_tr_name_input(TR_MEDIAN, image_name)
-        # Aca declaramos inputs necesarios para el handle. Este caso no tiene.
         build_tr_value_int_selector('n', 3, 255)
-        build_tr_radiobuttons(PaddingStrategy.values())
+        build_tr_radio_buttons(PaddingStrategy.values())
         build_tr_dialog_end_buttons(TR_MEDIAN, image_name, tr_median)
 
 def tr_median(image_name: str) -> Image:
@@ -431,7 +436,7 @@ def tr_median(image_name: str) -> Image:
     new_name: str = get_req_tr_name_value(image)
     n: int = int(dpg.get_value(TR_INT_VALUE_SELECTOR))
     padding: str = dpg.get_value(TR_RADIO_BUTTONS)
-    # 2. Procesamos - Puede ser async
+    # 2. Procesamos
     new_data = image.apply_over_channels(median, n, padding)
     # 3. Creamos Imagen
     return Image(new_name, image.format, new_data)
