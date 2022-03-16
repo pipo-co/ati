2b2b145fb1044f10f2972c1aaef653b778679e46
Tobias Brandy <tbrandy@itba.edu.ar>
2022-03-16 18:06:56 -0300

    denoising cleanup

--- denoising.py
+++ denoising.py
@@ -6,27 +6,27 @@ import numpy as np
 
 from numpy.lib.stride_tricks import sliding_window_view
 
+from image_utils import Image
+
 
 class PaddingStrategy(Enum):
     ZEROS   = functools.partial(lambda c, n: np.pad(c, n, mode='constant', constant_values=0))
     REFLECT = functools.partial(lambda c, n: np.pad(c, n, mode='reflect'))
     EDGE    = functools.partial(lambda c, n: np.pad(c, n, mode='edge'))
 
-    def pad(self, matrix: np.ndarray, shape: np.ndarray) -> np.ndarray:
+    def pad(self, matrix: np.ndarray, shape: Tuple[int, ...]) -> np.ndarray:
         return self.value(matrix, (shape[0] - 1) // 2)
 
     @classmethod
-    def values(cls):
+    def names(cls):
         return list(map(lambda c: c.name, cls))
 
     @classmethod
     def from_str(cls, strategy: str) -> 'PaddingStrategy':
         strategy_name = strategy.upper()
-
-        if strategy_name not in PaddingStrategy.values():
-            raise ValueError(f'"{strategy_name}" is not a supported padding strategy')
+        if strategy_name not in PaddingStrategy.names():
+            raise ValueError(f'"{strategy_name.capitalize()}" is not a supported padding strategy')
         return cls[strategy_name]
-
 
 def is_kernel_valid(kernel: np.ndarray) -> bool:
     return len(kernel.shape) == 2 \
@@ -34,31 +34,30 @@ def is_kernel_valid(kernel: np.ndarray) -> bool:
            and kernel.shape[0] % 2 == 1 \
            and kernel.shape[0] > 1
 
+def sliding_window(matrix: np.ndarray, shape: Tuple[int, ...], padding_str: PaddingStrategy) -> np.ndarray:
+    return sliding_window_view(padding_str.pad(matrix, shape), shape)
 
-def sliding_window(matrix: np.ndarray, shape: Tuple[int], padding_strategy: str) -> np.ndarray:
-    padded = PaddingStrategy.from_str(padding_strategy).pad(matrix, shape)
-    return sliding_window_view(padded, shape)
-
-
-def weighted_mean(channel: np.ndarray, kernel: np.ndarray, padding_strategy: str) -> np.ndarray:
+def weighted_mean(channel: np.ndarray, kernel: np.ndarray, padding_str: PaddingStrategy) -> np.ndarray:
     if not is_kernel_valid(kernel):
         raise ValueError(f'Invalid kernel: {kernel}')
-
-    sw = sliding_window(channel, kernel.shape, padding_strategy)
+    sw = sliding_window(channel, kernel.shape, padding_str)
     return np.mean(sw[:, :] * kernel, axis=(2, 3)).astype(np.uint8)
 
+def mean_channel(channel: np.ndarray, kernel_size: int, padding_str: PaddingStrategy) -> np.ndarray:
+    return weighted_mean(channel, np.full((kernel_size, kernel_size), 1), padding_str)
 
-def mean(channel: np.ndarray, n: int, padding_strategy: str) -> np.ndarray:
-    return weighted_mean(channel, np.full((n, n), 1), padding_strategy)
-
-
-def weighted_median(channel: np.ndarray, kernel: np.ndarray, padding_strategy: str) -> np.ndarray:
+def weighted_median(channel: np.ndarray, kernel: np.ndarray, padding_str: PaddingStrategy) -> np.ndarray:
     if not is_kernel_valid(kernel):
         raise ValueError(f'Invalid kernel: {kernel}')
-
-    sw = sliding_window(channel, kernel.shape, padding_strategy)
+    sw = sliding_window(channel, kernel.shape, padding_str)
     return np.median(sw[:, :] * kernel, axis=(2, 3)).astype(np.uint8)
 
+def median_channel(channel: np.ndarray, kernel_size: int, padding_str: PaddingStrategy) -> np.ndarray:
+    return weighted_median(channel, np.full((kernel_size, kernel_size), 1), padding_str)
 
-def median(channel: np.ndarray, n: int, padding_strategy: str) -> np.ndarray:
-    return weighted_median(channel, np.full((n, n), 1), padding_strategy)
+# ******************* Export Functions ********************** #
+def mean(image: Image, kernel_size: int, padding_str: PaddingStrategy) -> np.ndarray:
+    return image.apply_over_channels(mean_channel, kernel_size, padding_str)
+
+def median(image: Image, kernel_size: int, padding_str: PaddingStrategy) -> np.ndarray:
+    return image.apply_over_channels(median_channel, kernel_size, padding_str)
