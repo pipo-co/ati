e8ce0ecd4b820fc390edccd572e9040388d02c69
Tobias Brandy <tbrandy@itba.edu.ar>
2022-05-11 10:10:02 -0300

    Idea interfaz draw command

---
+++ models/draw_cmd.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Union
+
+import numpy as np
+
+
+@dataclass
+class LineDrawCmd:
+    p1_x: int
+    p1_y: int
+    p2_x: int
+    p2_y: int
+
+
+@dataclass
+class CircleDrawCmd:
+    c_x: int
+    c_y: int
+    r: float
+
+@dataclass
+class ScatterDrawCmd:
+    points: np.ndarray
+
+DrawCmd = Union[LineDrawCmd, CircleDrawCmd, ScatterDrawCmd]
+
